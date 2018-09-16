45acb41b6607682bd4aaa3c2a05e0c4f54e14163
Gustavo Calderon <g.calderonxd@gmail.com>
2018-09-16 13:07:32 -0400

    refactor initial validation Refactor initial unhandled exceptions.

--- app/lib/logger/index.js
+++ app/lib/logger/index.js
@@ -3,30 +3,29 @@ const winston = require('winston');
 const { combine, timestamp, printf } = winston.format;
 
 const myFormat = printf(info => {
-  let format = `${info.timestamp}`;
+  const format = `${info.timestamp}`;
   if (info instanceof Error) {
     return `${format} ${info.stack}`
   }
   return `${format} ${info.level}: ${info.message}`;
 });
 
-const replaceErrors = (value) => {
-  const error = {};
-
-  Object.getOwnPropertyNames(value).forEach((valuesKey) => {
-    error[valuesKey] = value[valuesKey];
-  });
-  return error;
-}
-
 const logger = winston.createLogger({
   format: combine(
     timestamp(),
-    // winston.format.json({ replacer: replaceErrors })
     myFormat
   ),
   level     : 'debug',
   transports: [new winston.transports.Console()]
 })
+
+process.on('uncaughtException', (ex) => {
+  logger.error(ex);
+  process.exit(1);
+})
+
+process.on('unhandledRejection', (ex) => {
+  throw ex;
+});
 
 module.exports = logger;
--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 require('express-async-errors');
+require('./startup/validation')();
 const express = require('express');
 const log     = require('./app/lib/logger');
-const config  = require('config');
 const app     = express();
 require('./startup/routes')(app);
-
-process.on('unhandledRejection', (ex) => {
-  log.error(ex.message, ex);
-  process.exit(1);
-});
-
-if (!config.get('jwtPrivateKey')) {
-  log.error('FATAL: jwtPrivateKey is not defined');
-  process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port);
